a9d4bf06605736f0705a9a936b2e0d73dab7432e
Marc <marc@highcard.dev>
2022-04-14 23:06:27 +0200

    fix: tests

--- rcon_test.go
+++ rcon_test.go
@@ -206,7 +206,8 @@ func TestConn_Execute(t *testing.T) {
 		defer conn.Close()
 
 		result, err := conn.Execute("deadline")
-		wantErrMsg := fmt.Sprintf("rcon: read packet size: read tcp %s->%s: i/o timeout", conn.LocalAddr(), conn.RemoteAddr())
+		//wantErrMsg := fmt.Sprintf("rcon: read packet size: read tcp %s->%s: i/o timeout", conn.LocalAddr(), conn.RemoteAddr())
+		wantErrMsg := fmt.Sprintf("excecute timeout"), conn.RemoteAddr())
 		if err == nil || err.Error() != wantErrMsg {
 			t.Errorf("got err %q, want to contain %q", err, wantErrMsg)
 		}
